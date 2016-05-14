567d0b44389361741fa0d5d4ac49851be7eb5a32
Sebastian Woetzel <wose@zuendmasse.de>
2016-05-15 01:20:26 +0200

    added imgui and simple example dialog

---
+++ src/main.cpp
@@ -0,0 +1,68 @@
+#include <imgui.h>
+#include "imgui_impl_glfw_gl3.h"
+#include <stdio.h>
+#include <GL/gl3w.h>
+#include <GLFW/glfw3.h>
+
+static void error_callback(int error, const char* description)
+{
+    fprintf(stderr, "Error %d: %s\n", error, description);
+}
+
+int main(int, char**)
+{
+    // Setup window
+    glfwSetErrorCallback(error_callback);
+    if (!glfwInit())
+        return 1;
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+#if __APPLE__
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+#endif
+    GLFWwindow* window = glfwCreateWindow(1280, 720, "HBVisionGraphEditor", NULL, NULL);
+    glfwMakeContextCurrent(window);
+    gl3wInit();
+
+    // Setup ImGui binding
+    ImGui_ImplGlfwGL3_Init(window, true);
+
+    ImVec4 clear_color = ImColor(30, 30, 30);
+
+    // Main loop
+    while (!glfwWindowShouldClose(window))
+    {
+        glfwPollEvents();
+        ImGui_ImplGlfwGL3_NewFrame();
+
+        {
+            static float f = 0.0f;
+            ImGui::Text("Hello, world!");
+            ImGui::SliderFloat("float", &f, 0.0f, 1.0f);
+            ImGui::ColorEdit3("clear color", (float*)&clear_color);
+            ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
+        }
+
+        // 2. Show another simple window, this time using an explicit Begin/End pair
+        ImGui::SetNextWindowSize(ImVec2(200,100), ImGuiSetCond_FirstUseEver);
+        ImGui::Begin("Image Source");
+        ImGui::Text("Hello");
+        ImGui::End();
+
+        // Rendering
+        int display_w, display_h;
+        glfwGetFramebufferSize(window, &display_w, &display_h);
+        glViewport(0, 0, display_w, display_h);
+        glClearColor(clear_color.x, clear_color.y, clear_color.z, clear_color.w);
+        glClear(GL_COLOR_BUFFER_BIT);
+        ImGui::Render();
+        glfwSwapBuffers(window);
+    }
+
+    // Cleanup
+    ImGui_ImplGlfwGL3_Shutdown();
+    glfwTerminate();
+
+    return 0;
+}
